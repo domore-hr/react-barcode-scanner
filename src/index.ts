a55776f6efa94848272173a52423df450e2d1573
Jakov Glavina <jakov.glavina@icloud.com>
2023-09-04 17:39:06 +0200

    chore: remove wasm polyfill usage

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { BarcodeDetectorPolyfill } from '@preflower/barcode-detector-polyfill'
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/semi */
+// import { BarcodeDetectorPolyfill } from "@undecaf/barcode-detector-polyfill";
 
 try {
   // @ts-expect-error
-  window.BarcodeDetector.getSupportedFormats()
+  window.BarcodeDetector.getSupportedFormats();
 } catch {
-  // @ts-expect-error
-  window.BarcodeDetector = BarcodeDetectorPolyfill
+  // // @ts-expect-error
+  // window.BarcodeDetector = BarcodeDetectorPolyfill;
 }
 
-export * from './hooks'
-export * from './components'
+export * from "./hooks";
+export * from "./components";
